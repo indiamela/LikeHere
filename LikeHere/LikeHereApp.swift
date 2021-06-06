e9e16541f60b801a5b73be1b0bbd6ef940c5b92b
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-06-06 17:07:12 +0900

    firstCommit

---
+++ LikeHere/LikeHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikeHereApp.swift
+//  LikeHere
+//
+//  Created by Taishi Kusunose on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct LikeHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
