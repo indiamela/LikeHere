3c0c5ab8b86ec26084a20bbe7e7b78086a09b821
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-06-18 08:31:13 +0900

    add-AppleAuthFile

--- LikeHere/LikeHereApp.swift
+++ LikeHere/LikeHereApp.swift
@@ -6,10 +6,16 @@
 //
 
 import SwiftUI
+import FirebaseCore
 import GoogleSignIn
 
 @main
 struct LikeHereApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
