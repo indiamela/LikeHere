aeca41045f2a60d7d010c8a114a0bf6932b3767e
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-07-06 07:51:25 +0900

    add-textEditView

--- LikeHere/Model/PostArrayObject.swift
+++ LikeHere/Model/PostArrayObject.swift
@@ -41,4 +41,9 @@ class PostArrayObject:ObservableObject{
         }
     }
     
+    init(userID: String) {
+        print("FETCH FROM DATABASE")
+        
+    }
+    
 }
--- LikeHere/Services/Firebase/DataService.swift
+++ LikeHere/Services/Firebase/DataService.swift
@@ -49,8 +49,5 @@ class DataService {
                 return
             }
         }
-        
-
-        
     }
 }
